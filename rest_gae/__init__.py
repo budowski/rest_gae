bd818fe8fffd5dd3146ba5acbd27f76c4778dbc8
budowski <budowski@gmail.com>
2014-01-30 03:26:11 +0200

    Updated version number

--- rest_gae/__init__.py
+++ rest_gae/__init__.py
@@ -2,3 +2,12 @@ from rest_gae import RESTHandler, PERMISSION_ANYONE, PERMISSION_LOGGED_IN_USER, 
 
 __all__ = ['RESTHandler', 'PERMISSION_ANYONE', 'PERMISSION_LOGGED_IN_USER', 'PERMISSION_OWNER_USER', 'PERMISSION_ADMIN']
 
+VERSION = (1, 0, 0)
+
+def get_version():
+    if isinstance(VERSION[-1], basestring):
+        return '.'.join(map(str, VERSION[:-1])) + VERSION[-1]
+    return '.'.join(map(str, VERSION))
+
+__version__ = get_version()
+
